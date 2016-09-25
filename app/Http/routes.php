7c6c6745c6ec4842244d918ab7edbd0d76ecd6b5
Freddy Chew <Freddy Chew>
2016-09-25 16:33:58 -0500

    educatoros

--- app/Http/routes.php
+++ app/Http/routes.php
@@ -62,6 +62,10 @@ Route::get('laravel-version', function()
 Route::get('/volunteers/interestform', 'InterestformsController@interestform');
 Route::post('/volunteers/interestform', 'InterestformsController@store');
 
+Route::get('/educators/introduction', 'EducatorsController@index');
+
+
+
 //    Route::delete('/comments/{comment}', 'CommentsController@destroy');
 //    Route::resource('comments', 'CommentsController');
 //    Route::get('comments/{student}/addforstudent', ['as' => 'comments.addforstudent',
