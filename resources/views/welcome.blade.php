0bb5c86fead8566680eaa6b6217bce5685630a86
Jeff Gann <jdgann@unomaha.edu>
2016-09-27 17:43:32 -0500

    added static content to homepage

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -1,50 +1,77 @@
 @extends('layouts.app')
 
 @section('content')
-<div class="container">
-    <div class="row">
-        <div class="col-md-15 col-md-offset-0">
-            <div class="panel panel-default">
-                <p id="P_1">
-                    JA's volunteer-delivered, kindergarten-12th grade programs foster <strong id="STRONG_2">work-readiness, entrepreneurship</strong> and <strong id="STRONG_3">financial literacy skills,</strong> and use experiential learning to inspire students to dream big and reach their potential.
-                </p>
-
-                <div style="text-align: center;"><img src="images/two-cols-classroom.jpg"></div>
-                <!--
-                <div class="panel-body">
-                    Place some welcome text here for the your Basic Starter Application and maybe a description about intended use.
+    <div class="container">
+        <div class="row">
+            <div class="col-md-15 col-md-offset-0">
+                <div class="panel panel-default">
+                    <p id="P_1">JA's volunteer-delivered, kindergarten-12th
+                    grade programs foster <strong id="STRONG_2">work-readiness,
+                    entrepreneurship</strong> and <strong id=
+                    "STRONG_3">financial literacy skills,</strong> and use
+                    experiential learning to inspire students to dream big and
+                    reach their potential.</p>
                 </div>
-                -->
             </div>
         </div>
-    </div>
-<div ng-cloak="" class="tabsdemoDynamicHeight">
-  <md-content>
-    <md-tabs md-dynamic-height="" md-border-bottom="">
-      <md-tab label="one">
-        <md-content class="md-padding">
-          <h1 class="md-display-2">Tab One</h1>
-          <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nulla venenatis ante augue. Phasellus volutpat neque ac dui mattis vulputate. Etiam consequat aliquam cursus. In sodales pretium ultrices. Maecenas lectus est, sollicitudin consectetur felis nec, feugiat ultricies mi.</p>
-        </md-content>
-      </md-tab>
-      <md-tab label="two">
-        <md-content class="md-padding">
-          <h1 class="md-display-2">Tab Two</h1>
-          <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nulla venenatis ante augue. Phasellus volutpat neque ac dui mattis vulputate. Etiam consequat aliquam cursus. In sodales pretium ultrices. Maecenas lectus est, sollicitudin consectetur felis nec, feugiat ultricies mi. Aliquam erat volutpat. Nam placerat, tortor in ultrices porttitor, orci enim rutrum enim, vel tempor sapien arcu a tellus. Vivamus convallis sodales ante varius gravida. Curabitur a purus vel augue ultrices ultricies id a nisl. Nullam malesuada consequat diam, a facilisis tortor volutpat et. Sed urna dolor, aliquet vitae posuere vulputate, euismod ac lorem. Sed felis risus, pulvinar at interdum quis, vehicula sed odio. Phasellus in enim venenatis, iaculis tortor eu, bibendum ante. Donec ac tellus dictum neque volutpat blandit. Praesent efficitur faucibus risus, ac auctor purus porttitor vitae. Phasellus ornare dui nec orci posuere, nec luctus mauris semper.</p>
-          <p>Morbi viverra, ante vel aliquet tincidunt, leo dolor pharetra quam, at semper massa orci nec magna. Donec posuere nec sapien sed laoreet. Etiam cursus nunc in condimentum facilisis. Etiam in tempor tortor. Vivamus faucibus egestas enim, at convallis diam pulvinar vel. Cras ac orci eget nisi maximus cursus. Nunc urna libero, viverra sit amet nisl at, hendrerit tempor turpis. Maecenas facilisis convallis mi vel tempor. Nullam vitae nunc leo. Cras sed nisl consectetur, rhoncus sapien sit amet, tempus sapien.</p>
-          <p>Integer turpis erat, porttitor vitae mi faucibus, laoreet interdum tellus. Curabitur posuere molestie dictum. Morbi eget congue risus, quis rhoncus quam. Suspendisse vitae hendrerit erat, at posuere mi. Cras eu fermentum nunc. Sed id ante eu orci commodo volutpat non ac est. Praesent ligula diam, congue eu enim scelerisque, finibus commodo lectus.</p>
-        </md-content>
-      </md-tab>
-      <md-tab label="three">
-        <md-content class="md-padding">
-          <h1 class="md-display-2">Tab Three</h1>
-          <p>Integer turpis erat, porttitor vitae mi faucibus, laoreet interdum tellus. Curabitur posuere molestie dictum. Morbi eget congue risus, quis rhoncus quam. Suspendisse vitae hendrerit erat, at posuere mi. Cras eu fermentum nunc. Sed id ante eu orci commodo volutpat non ac est. Praesent ligula diam, congue eu enim scelerisque, finibus commodo lectus.</p>
-        </md-content>
-      </md-tab>
-    </md-tabs>
-  </md-content>
+
+<div class="row">
+  <div class="col-md-4 box"></div>
+  <div class="col-md-4 box">
+    <a href="{{ url('/get_Involved/getinvolved') }}" id="A_1">Get Involved</a></div>
+  <div class="col-md-4 box"></div>
 </div>
+
+<div class="row">
+  <div class="col-md-2"></div>
+  <div class="col-md-6">
+    <iframe allowfullscreen=""
+                    frameborder="0" height="315" id="VID_1" name="VID_1" src=
+                    "https://www.youtube.com/embed/4L49IjKV6po" width=
+                    "560"></iframe></div>
+  <div class="col-md-3"></div>
 </div>
+
+<div class="row" id="WHY_1">
+  <div class="col-sm-6">
+                    <h2 id="H2_6">
+                        Why Junior Achievement?
+                    </h2>
+                    <p id="P_7">
+                        JA helps students realize that the education they are getting today will help them to have a bright future tomorrow. JA's unique, volunteer delivered programs, show them all of the possibilities that lay before them. They realize they can choose different paths; College? A specific trade? Start their own business? Through your participation as an organization or as an individual, these statistics below can begin to change in your community:
+                    </p>
+                    <ul id="UL_8">
+                        <li id="LI_9">
+                            20% of U.S. students will not complete high school on time and earn a diploma.
+                        </li>
+                        <li id="LI_10">
+                            49% of U.S. employers recognize that talent shortages impact their ability to serve clients and customers.
+                        </li>
+                        <li id="LI_11">
+                            36% of Americans say that they have at some point in their lives felt their financial situation was out of control.
+                        </li>
+                        <li id="LI_12">
+                            91% of millennials wish they had greater access to entrepreneurial education programs.
+                        </li>
+                    </ul>
+
+  </div>
+  <div class="col-sm-6"><img alt='' id="IMG_1" src="images/two-cols-classroom.jpg"></div>
+</div>
+<!--
+<div class="row">
+  <div class="col-md-4 box"></div>
+  <div class="col-md-4 box">
+    <a href="https://www.youtube.com/channel/UCgMAAachC-yG8p8r06S938A"><i class="fa fa-youtube fa-6"></i></a>
+  </div>
+  <div class="col-md-4 box">
+    <a href="https://www.facebook.com/JuniorAchievementUSA/"><i class="fa fa-facebook fa-6"></a>
+  </div>
+  <div class="col-md-4 box"></div>
+</div>
+-->
+</div>
+
 
 @endsection
 
