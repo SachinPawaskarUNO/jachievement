60e5f33f9bef09347ff8928c68701dd3c985e7ba
jeff <jdgann@unomaha.edu>
2016-10-01 15:41:58 -0500

    updated images url

--- resources/views/common/nav.blade.php
+++ resources/views/common/nav.blade.php
@@ -11,7 +11,7 @@
             </button>
 
             <!-- Branding Image -->
-            <div class="pull-left"><a href="{{ url('/') }}" id="ja_nav_home"><img src="images/ja_nav_logo.jpg" style="height: 48px;"></a></div>
+            <div class="pull-left"><a href="{{ url('/') }}" id="ja_nav_home"><img src="{{ url('/images/ja_nav_logo.jpg') }}" style="height: 48px;"></a></div>
 
         </div>
 
