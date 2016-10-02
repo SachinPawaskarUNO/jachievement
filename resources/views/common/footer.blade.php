51a5a3ebfc749198d8f876ad21dd3710823b60fc
Jeff Gann <jdgann@unomaha.edu>
2016-10-02 15:44:55 -0500

    added footer

---
+++ resources/views/common/footer.blade.php
@@ -0,0 +1,15 @@
+<div class="container-fluid">
+    <div class="container">
+      <br>
+      <br>
+      <div class="text-center">
+        <a href="https://www.youtube.com/watch?v=4L49IjKV6po"><i class="fa fa-youtube fa-5x" aria-hidden="true"></i></a>
+      <a href="https://www.facebook.com/pages/Junior-Achievement-of-The-Midlands/100873483290685"><i class="fa fa-facebook fa-5x" aria-hidden="true"></i></a>
+    </div>
+  </div>
+    <div class="container">
+      <div class="text-center">
+        <p class="p_foot" style="margin: 10px 0;">© Junior Achievement USA<sup>®</sup>&nbsp;/&nbsp;13506 West Maple Road,&nbsp;Omaha&nbsp;NE&nbsp;68164&nbsp;/&nbsp;Phone:&nbsp;402-333-6410&nbsp;/&nbsp;Fax:&nbsp;402-333-6797</p>
+    </div>
+  </div>
+</div>
--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -17,7 +17,7 @@
     <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/twitter-bootstrap/3.3.6/css/bootstrap.min.css" integrity="sha384-1q8mTJOASx8j1Au+a5WDVnPi2lkFfwwEAa8hDDdjZlpLegxhjVME1fgjWPGmkzs7" crossorigin="anonymous">
     <link rel="stylesheet" href="https://cdn.datatables.net/1.10.12/css/dataTables.bootstrap.min.css">
     <link rel="stylesheet" href="http://ajax.googleapis.com/ajax/libs/angular_material/1.1.0/angular-material.min.css">
-    <link rel="stylesheet" href="css/all.css">
+    <link rel="stylesheet" href="{{ url('css/all.css') }}">
     <!--<link href="{{ elixir('css/all.css') }}" rel="stylesheet">-->
     <link rel="stylesheet" href="https://fonts.googleapis.com/css?family=Open+Sans:300italic,400italic,600italic,700italic,800italic,400,300,600,700,800">
 
@@ -96,5 +96,6 @@
 
     <!-- Footer -->
     @yield('footer');
+    @include('common.footer');
 </body>
 </html>
