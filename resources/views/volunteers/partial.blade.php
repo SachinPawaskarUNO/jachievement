9f59e8bdb30d59f7a941794e1e784d1c0020db0b
arpitaadhikari <arpitaadhikari@unomaha.edu>
2016-10-03 18:25:26 -0500

    worked on css for button and panel

--- resources/views/volunteers/partial.blade.php
+++ resources/views/volunteers/partial.blade.php
@@ -261,6 +261,6 @@
 
     <div class="form-group">
         <div class="col-md-6 col-md-offset-4">
-            {!! Form::button('<i class="fa fa-btn fa-save"></i>Save', ['type' => 'submit','id' => 'save', 'class' => 'btn btn-primary']) !!}
+            {!! Form::button('<i class="fa fa-btn fa-save"></i>Save', ['type' => 'submit','id' => 'save', 'class' => 'btn btn-success']) !!}
         </div>
     </div>
