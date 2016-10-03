db4a45cefb8e71bb7b6ff807d8dac0aa79f3abb1
Ninh Nguyen <nhnguyen@unomaha.edu>
2016-10-03 09:09:49 -0500

    Create donors table

---
+++ database/migrations/2016_10_03_135530_create_donors_table.php
@@ -0,0 +1,36 @@
+<?php
+
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class CreateDonorsTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('donors', function (Blueprint $table) {
+            $table->increments('id');
+            $table->string('firstName');
+            $table->string('lastName');
+            $table->string('email');
+            $table->string('created_by')->default('System');
+            $table->string('updated_by')->default('System');
+            $table->integer('user_id')->unsigned()->nullable();
+            $table->foreign('user_id')->references('id')->on('users');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::drop('donors');
+    }
+}
