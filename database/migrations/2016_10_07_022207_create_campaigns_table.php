fda2e3d7da2e8af7d73dc065ac9fdcfdb0d29578
Penu <penu@Dixants-MacBook-Pro.local>
2016-10-09 16:19:16 -0500

    update

---
+++ database/migrations/2016_10_07_022207_create_campaigns_table.php
@@ -0,0 +1,46 @@
+<?php
+
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class CreateCampaignsTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('campaigns', function (Blueprint $table) {
+            $table->increments('id');
+            $table->string('name');
+            $table->string('description')->nullable();
+            $table->integer('goal');
+            $table->string('category');
+            $table->date('start_date');
+            $table->date('end_date');
+            $table->boolean('active')->default(true);
+
+            $table->integer('user_id')->unsigned()->nullable();
+            $table->foreign('user_id')->references('id')->on('users');
+
+            $table->string('created_by')->default('System');
+            $table->string('updated_by')->default('System');
+
+
+            $table->timestamps();
+            $table->softDeletes();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::drop('campaigns');
+    }
+}
