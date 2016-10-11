357013e8ba0a737d5e688acce3a1f470b0d26e87
Ninh Nguyen <nhnguyen@unomaha.edu>
2016-10-11 08:55:51 -0500

    add donation form

--- resources/views/donation/donate.blade.php
+++ resources/views/donation/donate.blade.php
@@ -1,16 +1,20 @@
 @extends('layouts.app')
 
 @section('content')
-
     <div class="container">
         <div class="row">
-
-            <div class="panel panel-default">
-                <div class="panel-heading"> {{ $heading }}</div>
-                DONE WITH SOW
-
+            <div class="col-md-8 col-md-offset-2">
+                <div class="panel panel-default">
+                    <div class="panel-heading" style="background-color:#5cb85c !important;"><span style="font-size:1.2em;"><b> {{ $heading }}</b></span></div>
+                    <div class="panel-body">
+                        {!! Form::open(['url' => '/donation/donate', 'class' => 'form-horizontal', 'method' => 'POST']) !!}
+                        @include('common.errors')
+                        @include('common.flash')
+                        @include ('donation.partial')
+                        {!! Form::close() !!}
+                    </div>
+                </div>
             </div>
-
         </div>
     </div>
 @endsection
