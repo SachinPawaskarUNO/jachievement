6c0631d15643c2b38cf2013e626a54d644b40b9b
Ninh Nguyen <nhnguyen@unomaha.edu>
2016-10-13 18:29:05 -0500

    button layout improvements

--- resources/views/donation/donate.blade.php
+++ resources/views/donation/donate.blade.php
@@ -3,7 +3,8 @@
 @section('content')
     <div class="container">
         <div class="row">
-            <div class="col-md-8 col-md-offset-2">
+
+            <div class="col-md-7 col-sm-6-pull">
                 <div class="panel panel-default">
                     <div class="panel-heading" style="background-color:#5cb85c !important;"><span style="font-size:1.2em;"><b> {{ $heading }}</b></span></div>
                     <div class="panel-body">
@@ -13,6 +14,21 @@
                         @include ('donation.partial')
                         {!! Form::close() !!}
                     </div>
+                </div>
+            </div>
+            <div class="col-md-5 col-sm-6-push">
+                <div class="row">
+                    <h3 class="text-center">Top 5 Contributors</h3>
+                    <ol class="list-group">
+                        <li class="list-group-item">John Wick: $500</li>
+                        <li class="list-group-item">Steve Trevor: $250</li>
+                        <li class="list-group-item">Felicity Smoak: $200</li>
+                        <li class="list-group-item">Steve Bob: $100</li>
+                        <li class="list-group-item">Thea Quinn: $50</li>
+                    </ol>
+                </div>
+                <div class="row container">
+                    <img class="img-responsive" id="IMG" alt="Beatiful Image" src="images/two-cols-classroom.jpg">
                 </div>
             </div>
         </div>
