47b8262c9de0713d59f86bb819b3d7599dd23c45
Ravi Agarwal <Ravi Agarwal>
2016-10-15 13:13:16 -0500

    updated teammember page

--- database/seeds/TeamMemberSeeder.php
+++ database/seeds/TeamMemberSeeder.php
@@ -14,12 +14,12 @@ class TeammembersTableSeeder extends Seeder
     public function run()
     {
         DB::table('teammembers')->delete();
-        Teammember::create(['id' => '1',  'goal' => '$250', 
+        Teammember::create([ 'goal' => '$250', 
             'created_by' => 'System', 'updated_by' => 'System', 'created_at' => date_create(), 'updated_at' => date_create(),'user_id'=>'31','team_id'=>'1']);
-        Teammember::create([ 'id'=> '2', 'goal' => '$300', 
-         'created_by' => 'System', 'updated_by' => 'System', 'created_at' => date_create(), 'updated_at' => date_create(),'user_id'=>'32','team_id'=>'2')]);
-        Teammember::create([  'id' => '3','goal' => '$450', 
-         'created_by' => 'System', 'updated_by' => 'System', 'created_at' => date_create(), 'updated_at' => date_create(),'user_id'=>'33','team_id'=>'3']);
+        Teammember::create([ 'goal' => '$300', 
+            'created_by' => 'System', 'updated_by' => 'System', 'created_at' => date_create(), 'updated_at' => date_create(),'user_id'=>'32','team_id'=>'2']);
+        Teammember::create([ 'goal' => '$450', 
+            'created_by' => 'System', 'updated_by' => 'System', 'created_at' => date_create(), 'updated_at' => date_create(),'user_id'=>'33','team_id'=>'3']);
 
     }
 }
