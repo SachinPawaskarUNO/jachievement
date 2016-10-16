97e9a582ad6eb13b4430bf7df67afeda1cac1f25
Ninh Nguyen <nhnguyen@unomaha.edu>
2016-10-16 17:35:55 -0500

    fix migration

--- database/migrations/2016_10_15_204343_add_foreign_keys_for_donations_table.php
+++ database/migrations/2016_10_15_204343_add_foreign_keys_for_donations_table.php
@@ -14,22 +14,19 @@ class AddForeignKeysForDonationsTable extends Migration
     {
          Schema::table('donations', function (Blueprint $table) {
            
-            $table->integer('team_id')->unsigned();
-            $table->foreign('team_id')->references('teams')->on('id');
+            $table->integer('team_id')->unsigned()->nullable();
+            $table->foreign('team_id')->references('id')->on('teams');
             
-            $table->integer('campaign_id')->unsigned();
-            $table->foreign('campaign_id')->references('campaigns')->on('id');
+            $table->integer('campaign_id')->unsigned()->nullable();
+            $table->foreign('campaign_id')->references('id')->on('campaigns');
             
-            $table->integer('teammember_id')->unsigned();
-            $table->foreign('teammember_id')->references('team_members')->on('id');
-
-            $table->integer('state_id')->unsigned();
-            $table->foreign('state_id')->references('states')->on('id');
+            $table->integer('teammember_id')->unsigned()->nullable();
+            $table->foreign('teammember_id')->references('id')->on('team_members');
             
         });
         Schema::table('donors', function (Blueprint $table) {
             $table->integer('state_id')->unsigned();
-            $table->foreign('state_id')->references('states')->on('id');
+            $table->foreign('state_id')->references('id')->on('states');
 
         });
     }
