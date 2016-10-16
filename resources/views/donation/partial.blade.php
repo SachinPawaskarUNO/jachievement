f9e9577a4a4d0d0ea65bfc11ac0d29ed9a19c9f9
Ninh Nguyen <nhnguyen@unomaha.edu>
2016-10-16 16:38:27 -0500

    commit changes

--- resources/views/donation/partial.blade.php
+++ resources/views/donation/partial.blade.php
@@ -38,11 +38,11 @@
             {!! Form::radio('amount', 200, ['id'=> 'amount5','class' => 'col-md-6 form-control']) !!}
             {!! Form::label('lb_1','$200',['class' => 'btn']) !!}
             </div>
-{{--            <div class="form-group">
-                {!! Form::radio('amount', null, ['id'=> 'amount_other','class' => 'col-md-6 form-control']) !!}
+            <div class="form-group">
+                {!! Form::radio('amount', 'other', ['id'=> 'amount_other','class' => 'col-md-6 form-control']) !!}
                 {!! Form::label('lb_1','Other:',['class' => 'btn']) !!}
-                {!! Form::text('amount', null, ['id'=> 'amount_actual','class' => 'form-control']) !!}
-            </div>--}}
+                {!! Form::text('otherAmount', null, ['id'=> 'amount_actual','class' => 'form-control']) !!}
+            </div>
         </div>
         @if ($errors->has('amount'))
             <span class="help-block">
@@ -102,14 +102,12 @@
     </div>
 </div>
 
-<div class="form-group{{ $errors->has('state') ? ' has-error' : '' }}">
-    {!! Form::label('state', 'State:', ['class' => 'col-md-4 control-label']) !!}
-    <span style="color:red;">*</span>
+<div class="form-group{{ $errors->has('state_id') ? ' has-error' : '' }}">
+    {!! Form::label('state_id', 'State:', ['class' => 'col-md-4 control-label']) !!}
     <div class="col-md-6">
-        {!! Form::text('state', null, ['id'=> 'state','class' => 'col-md-6 form-control']) !!}
-        @if ($errors->has('state'))
-            <span class="help-block"><strong>
-                    {{ $errors->first('state') }}</strong></span>
+        {!! Form::select('state_id', $states, null,  ['id'=> 'state_id','class' => 'col-md-6 form-control']) !!}
+        @if ($errors->has('state_id'))
+            <span class="help-block"><strong>{{ $errors->first('state_id') }}</strong></span>
         @endif
     </div>
 </div>
