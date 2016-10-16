1ab1e0c5e5b192682635463e6a8e1e30bc4b33bf
Ravi Agarwal <Ravi Agarwal>
2016-10-16 14:35:51 -0500

    updated migration file

--- app/Http/Controllers/CampaignController.php
+++ app/Http/Controllers/CampaignController.php
@@ -6,14 +6,20 @@ use Illuminate\Http\Request;
 
 use App\Http\Requests;
 use Log;
+use DB;
 
 class CampaignController extends Controller
 {
   public function teammember()
   {
       Log::info('CampaignController.teammember: ');
-        $this->viewData['heading'] = "Welcome to My Fundraising Page";
-      return view('campaign.teammember', $this->viewData);
+        $teamMembers= DB::table('team_members')
+                    ->join('donations', 'team_members.id', '=', 'donations.teammember_id')
+                    ->join('users', 'team_members.user_id', '=', 'users.id')
+                    ->select(DB::raw('team_members.id as id, team_members.goal as goal,donations.amount as amount,users.name as name'))
+                    ->get();
+        
+      return view('campaign.teammember', compact('teamMembers'));
       
   }
 
---
+++ database/migrations/2016_10_15_204343_add_foreign_keys_for_donations_table.php
@@ -0,0 +1,38 @@
+<?php
+
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class AddForeignKeysForDonationsTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+         Schema::table('donations', function (Blueprint $table) {
+           
+            $table->integer('team_id')->unsigned();
+            $table->foreign('team_id')->references('teams')->on('id');
+            
+            $table->integer('campaign_id')->unsigned();
+            $table->foreign('campaign_id')->references('campaigns')->on('id');
+            
+            $table->integer('teammember_id')->unsigned();
+            $table->foreign('teammember_id')->references('team_members')->on('id');
+            
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+         Schema::dropForeign(['team_id','campaign_id','teammember_id']);
+    }
+}
