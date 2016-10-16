b978b8d83a4cc330c150b0087fa6759f0cf2934b
Ninh Nguyen <nhnguyen@unomaha.edu>
2016-10-16 10:54:26 -0500

    fix database configuration

--- config/database.php
+++ config/database.php
@@ -32,7 +32,7 @@ return [
     |
     */
 
-    'default' => env('DB_CONNECTION', 'pgsql'),
+    'default' => env('DB_CONNECTION', 'mysql'),
 
     /*
     |--------------------------------------------------------------------------
@@ -62,14 +62,13 @@ return [
         'mysql' => [
             'driver'    => 'mysql',
             'host'      => env('DB_HOST', 'localhost'),
-            'port'      => env('DB_PORT', '8889'),
+            'port'      => env('DB_PORT', '3306'),
             'database'  => env('DB_DATABASE', 'jachievement'),
             'username'  => env('DB_USERNAME', 'root'),
-            'password'  => env('DB_PASSWORD', 'root'),
+            'password'  => env('DB_PASSWORD', ''),
             'charset'   => 'utf8',
             'collation' => 'utf8_unicode_ci',
             'prefix'    => '',
-	    'unix_socket'   => '/Applications/MAMP/tmp/mysql/mysql.sock',
             'strict'    => false,
             'engine'    => null
         ],
