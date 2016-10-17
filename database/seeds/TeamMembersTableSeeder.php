233b879e99882d698443a1c0a19d4e5cf396890f
Ninh Nguyen <nhnguyen@unomaha.edu>
2016-10-16 22:36:40 -0500

    fix seeding

--- database/seeds/TeamMembersTableSeeder.php
+++ database/seeds/TeamMembersTableSeeder.php
@@ -13,11 +13,11 @@ class TeamMembersTableSeeder extends Seeder
     public function run()
     {
         DB::table('team_members')->delete();
-        TeamMember::create([ 'goal' => '$250', 
+        TeamMember::create([ 'goal' => '250',
             'created_by' => 'System', 'updated_by' => 'System', 'created_at' => date_create(), 'updated_at' => date_create(),'user_id'=>'1','team_id'=>'1']);
-        TeamMember::create([ 'goal' => '$300', 
+        TeamMember::create([ 'goal' => '300',
             'created_by' => 'System', 'updated_by' => 'System', 'created_at' => date_create(), 'updated_at' => date_create(),'user_id'=>'2','team_id'=>'2']);
-        TeamMember::create([ 'goal' => '$450', 
+        TeamMember::create([ 'goal' => '450',
             'created_by' => 'System', 'updated_by' => 'System', 'created_at' => date_create(), 'updated_at' => date_create(),'user_id'=>'3','team_id'=>'3']);
     }
 }
