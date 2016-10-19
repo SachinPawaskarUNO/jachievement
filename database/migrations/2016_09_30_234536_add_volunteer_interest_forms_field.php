cd3edbf5ad2cf051ef6e1e5eee679a17c37464dd
Ninh Nguyen <nhnguyen@unomaha.edu>
2016-10-19 09:59:45 -0500

    fix

--- database/migrations/2016_09_30_234536_add_volunteer_interest_forms_field.php
+++ database/migrations/2016_09_30_234536_add_volunteer_interest_forms_field.php
@@ -25,7 +25,7 @@ class AddVolunteerInterestFormsField extends Migration
     public function down()
     {
         Schema::table('volunteer_interest_forms', function (Blueprint $table) {
-            $table->dropColumn('modeOfContact');
+            $table->dropColumn('mode_of_contact');
         });
     }
 }
