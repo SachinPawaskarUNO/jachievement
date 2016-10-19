d10cf02024bb7aa020f3cfcaf1b7c22d2b7a075b
Ninh Nguyen <nhnguyen@unomaha.edu>
2016-10-19 11:53:45 -0500

    fix

--- resources/views/donation/partial.blade.php
+++ resources/views/donation/partial.blade.php
@@ -145,10 +145,10 @@
 </div>
 
 <div class="form-inline">
-    <div class="col-md-6 col-md-offset-3">
+    <div class="col-md-7 col-md-offset-3">
         <div class="form-group">
             {!! Form::checkbox('anonymous', 1, false, ['id'=> 'anonymous', 'class' => 'check_box' ]) !!}
-            {!! Form::label('I would like to have my donation anonymous') !!}
+            {!! Form::label('I would like to have my donation to be anonymous') !!}
         </div>
     </div>
 </div>
