8171e879be7a40cfd88addc7226f0903a077d44c
Ninh Nguyen <nhnguyen@unomaha.edu>
2016-10-18 19:51:55 -0500

    fix donation

--- app/Http/Requests/DonationRequest.php
+++ app/Http/Requests/DonationRequest.php
@@ -28,7 +28,7 @@ class DonationRequest extends Request
             'last_name' => 'required|max:100|regex:/^[a-z ,.\'-]+$/i',
             'address' => 'max:100',
             'city' => 'max:100|alpha',
-            'zip' => 'required|regex:/^\d{5}(?:[-\s]\d{4})?$/',
+            'zip' => 'regex:/^\d{5}(?:[-\s]\d{4})?$/',
             'phone' => 'regex:/\(?([0-9]{3})\)?([ .-]?)([0-9]{3})\2([0-9]{4})/',
             'email' => 'required|max:100|email',
             'amount_actual' => 'numeric|min:1|max:10000',
