f6a176923572a56fda04d02956a6081480bbcc44
arpitaadhikari <arpitaadhikari@unomaha.edu>
2016-10-23 13:56:52 -0500

    Worked on campaign team

--- app/Http/Controllers/CampaignController.php
+++ app/Http/Controllers/CampaignController.php
@@ -84,8 +84,20 @@ class CampaignController extends Controller
     public function team()
     {
         Log::info('CampaignController.team: ');
-        $this->viewData['heading'] = "Welcome to My Fundraising Page";
-        return view('campaign.team', $this->viewData);
+        //$this->viewData['heading'] = "Welcome to My Fundraising Page";
+
+
+        $teamMembers= DB::table('team_members')
+            ->select(DB::raw('users.name as name, team_members.goal as goal,
+                    sum(donations.amount) as amount,(sum(donations.amount)/team_members.goal) * 100 as per_raised'))
+            ->join('donations', 'team_members.id', '=', 'donations.team_member_id')
+            ->join('users', 'team_members.user_id', '=', 'users.id')
+            ->groupBy('users.name', 'team_members.goal')
+            ->orderBy('per_raised')
+            ->get();
+
+        return view('campaign.teammember', compact('teamMembers'));
+        //return view('campaign.team', $this->viewData);
 
     }
 
