995812e5b51a9bef65c9f5a929528c269caa028c
saipriyacherukuri <scherukuri@unomaha.edu>
2016-10-26 22:01:43 -0500

    fix campaign migration

--- database/seeds/CampaignsTableSeeder.php
+++ database/seeds/CampaignsTableSeeder.php
@@ -14,12 +14,13 @@ class CampaignsTableSeeder extends Seeder
     public function run()
     {
         DB::table('campaigns')->delete();
-        Campaign::create(['id' => '1',  'name' => 'JA Omaha Golf Campaign - 2016', 'description' => 'Donate for Golf','goal'=> '200','category' => 'Golf','start_date'=>'10/10/2016','end_date'=>'10/20/2016','active'=>'true',
-            'created_by' => 'System', 'updated_by' => 'System', 'created_at' => date_create(), 'updated_at' => date_create()]);
-        Campaign::create([ 'id'=> '2', 'name' => 'JA Omaha Bowling Campaign - 2016', 'description' => 'Donate for Bowling','goal'=>'350','category' => 'Bowling','start_date'=>'11/15/2016','end_date'=>'11/20/2016','active'=>'true',
-            'created_by' => 'System', 'updated_by' => 'System', 'created_at' => date_create(), 'updated_at' => date_create()]);
-        Campaign::create([  'id' => '3','name' => 'JA Omaha Soccer Campaign - 2016', 'description' => 'Donate for Soccer','goal'=> '400','category' => 'Soccer','start_date'=>'12/01/2016','end_date'=>'12/10/2016','active'=>'true',
-            'created_by' => 'System', 'updated_by' => 'System', 'created_at' => date_create(), 'updated_at' => date_create()]);
+        //Campaign::create(['id' => '1',  'name' => 'JA Omaha Golf Campaign - 2016', 'description' => 'Donate for Golf','goal'=> '200','category' => 'Golf','start_date'=>'10/10/2016','end_date'=>'10/20/2016','active'=>'true',            'created_by' => 'System', 'updated_by' => 'System', 'created_at' => date_create(), 'updated_at' => date_create()]);
+        //Campaign::create([ 'id'=> '2', 'name' => 'JA Omaha Bowling Campaign - 2016', 'description' => 'Donate for Bowling','goal'=>'350','category' => 'Bowling','start_date'=>'11/15/2016','end_date'=>'11/20/2016','active'=>'true','created_by' => 'System', 'updated_by' => 'System', 'created_at' => date_create(), 'updated_at' => date_create()]);
+        //Campaign::create([  'id' => '3','name' => 'JA Omaha Soccer Campaign - 2016', 'description' => 'Donate for Soccer','goal'=> '400','category' => 'Soccer','start_date'=>'12/01/2016','end_date'=>'12/10/2016','active'=>'true','created_by' => 'System', 'updated_by' => 'System', 'created_at' => date_create(), 'updated_at' => date_create()]);
+			
+		Campaign::create(['id' => '1',  'name' => 'Junior Achievement Omaha Golf Campaign - 2016', 'description' => 'Donate for our Golf Campaign','goal'=> '200','start_date'=>'10/10/2016','end_date'=>'10/20/2016','active'=>'true','created_by' => 'System', 'updated_by' => 'System', 'created_at' => date_create(), 'updated_at' => date_create()]);
+        Campaign::create([ 'id'=> '2', 'name' => 'Junior Achievement Omaha Bowling Campaign - 2016', 'description' => 'Donate for our Bowling Campaign','goal'=>'350','start_date'=>'11/15/2016','end_date'=>'11/20/2016','active'=>'true','created_by' => 'System', 'updated_by' => 'System', 'created_at' => date_create(), 'updated_at' => date_create()]);
+        Campaign::create([  'id' => '3','name' => 'Junior Achievement Omaha Soccer Campaign - 2016', 'description' => 'Donate for our Soccer Campaign','goal'=> '400','start_date'=>'12/01/2016','end_date'=>'12/10/2016','active'=>'true','created_by' => 'System', 'updated_by' => 'System', 'created_at' => date_create(), 'updated_at' => date_create()]);
 
     }
 }
