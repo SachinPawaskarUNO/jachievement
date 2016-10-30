0871d7a907b3d05dd0f2dd7f427a9374953f8e27
Ninh Nguyen <nhnguyen@unomaha.edu>
2016-10-30 10:11:31 -0500

    add javascript package

--- config/app.php
+++ config/app.php
@@ -151,7 +151,7 @@ return [
         Illuminate\Translation\TranslationServiceProvider::class,
         Illuminate\Validation\ValidationServiceProvider::class,
         Illuminate\View\ViewServiceProvider::class,
-
+	'Laracasts\Utilities\JavaScript\JavaScriptServiceProvider',
         /*
          * Application Service Providers...
          */
