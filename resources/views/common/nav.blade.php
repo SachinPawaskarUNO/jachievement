90a42ef2b8807f6b63e4f9ccddc4d0f2e18b6ff7
jmiverson <jiverson@unomaha.edu>
2016-11-06 21:08:24 -0600

    Formatting Administration nav menu

--- resources/views/common/nav.blade.php
+++ resources/views/common/nav.blade.php
@@ -19,7 +19,7 @@
                     <!-- Left Side Of Navbar --> 
             <ul class="nav navbar-nav">
                 <li class="dropdown">
-                        <a id="ja_nav_about" href="#" class="dropdown-toggle" data-toggle="dropdown" role="button" aria-expanded="false">About Us<span class="caret"></span></a>
+                        <a id="ja_nav_about" href="#" class="dropdown-toggle" data-toggle="dropdown" role="button" aria-expanded="false">About Us <span class="caret"></span></a>
                     <ul class="dropdown-menu" role="menu">
                             <li><a id="ja_nav_purpose" href="{{ url('/aboutus/index') }}">Mission</a></li>
                             <li><a id="ja_nav_programs" href="{{ url('/programs/index') }}">Programs</a></li>
@@ -29,7 +29,7 @@
                     </ul>
                 </li>
                 <li class="dropdown">
-                        <a id="ja_nav_getinv" href="#" class="dropdown-toggle" data-toggle="dropdown" role="button" aria-expanded="false">Get Involved<span class="caret"></span></a>
+                        <a id="ja_nav_getinv" href="#" class="dropdown-toggle" data-toggle="dropdown" role="button" aria-expanded="false">Get Involved <span class="caret"></span></a>
                     <ul class="dropdown-menu" role="menu">
                             <li><a id="ja_nav_educator" href="{{ url('/educators/introduction') }}">Educators</a></li>
                             <li><a id="ja_nav_volunteer" href="{{ url('/volunteers/introduction') }}">Volunteers</a></li>
@@ -38,7 +38,7 @@
                     </ul>
                 </li>
                 <li class="dropdown">
-                        <a id="ja_nav_getinv" href="#" class="dropdown-toggle" data-toggle="dropdown" role="button" aria-expanded="false">Sign Up<span class="caret"></span></a>
+                        <a id="ja_nav_getinv" href="#" class="dropdown-toggle" data-toggle="dropdown" role="button" aria-expanded="false">Sign Up <span class="caret"></span></a>
                     <ul class="dropdown-menu" role="menu">
                             <li><a id="ja_nav_educator" href="{{ url('/educators/interestform') }}">As An Educator</a></li>
                             <li><a id="ja_nav_volunteer" href="{{ url('/volunteers/interestform') }}">As A Volunteer</a></li>
@@ -57,12 +57,15 @@
                 @role('admin')
                 <li class="dropdown">
                     <a href="#" id="ja_nav_getinv" class="dropdown-toggle" data-toggle="dropdown" role="button" aria-expanded="false">
-                        Administration<span class="caret"></span></a>
+                        Administration <span class="caret"></span></a>
                     <ul class="dropdown-menu multi level" role="menu">
-                        <li><a id="ja_nav_users" href="{{ url('/users') }}">Users</a></li>
-                        <li><a id="ja_nav_roles" href="{{ url('/roles') }}">Roles</a></li>
-                        <li><a id="ja_nav_educator_form" href="{{ url('/admin/educatorform') }}">Educator Forms</a></li>
-                        <li><a id="ja_nav_volunteer_form" href="{{ url('/admin/volunteerform') }}">Volunteer Forms</a></li>
+                        <li><a id="ja_nav_users" href="{{ url('/users') }}">Manage Users</a></li>
+                        <li><a id="ja_nav_roles" href="{{ url('/roles') }}">Manage Roles</a></li>
+                        <li><a id="ja_nav_schools" href="{{ url('/schools') }}">Manage Schools</a></li>
+                        <li role="separator" class="divider"></li>
+                        <li><a id="ja_nav_educator_form" href="{{ url('/admin/educatorform') }}">View Educators</a></li>
+                        <li><a id="ja_nav_volunteer_form" href="{{ url('/admin/volunteerform') }}">View Volunteers</a></li>
+                        <li role="separator" class="divider"></li>
                         <li><a id="ja_reports_donation" href="{{ url('/reports/donation') }}">Donation Reports</a></li>
                     </ul>
                 </li>
@@ -79,9 +82,10 @@
                     <li class="dropdown">
                         <a href="#" class="dropdown-toggle" data-toggle="dropdown" role="button" aria-expanded="false">Welcome, {{ Auth::user()->first_name }} <span class="caret"></span></a>
                         <ul class="dropdown-menu" role="menu">
+                            <li><a id="ja_changepw" href="{{ url('/change-password') }}">Change Password</a></li>
+                            <li><a id="ja_teamview" href="{{ url('/event/team/view') }}">View My Teams</a></li>
+                            <li role="separator" class="divider"></li>
                             <li><a id="ja_logout" href="{{ url('/logout') }}">Logout</a></li>
-                            <li><a id="ja_changepw"href="{{ url('/change-password') }}">Change Password</a></li>
-                             <li><a id="ja_teamview" href="{{ url('/event/team/view') }}">View My Teams</a></li>
                         </ul>
                     </li>
                 @endif
