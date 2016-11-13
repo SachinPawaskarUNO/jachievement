bc98ed828382332926503c6c549b19479ddba19d
saipriyacherukuri <scherukuri@unomaha.edu>
2016-11-13 16:58:30 -0600

    bug fixes on the Event Pages

--- resources/views/event/eventmarketing.blade.php
+++ resources/views/event/eventmarketing.blade.php
@@ -27,7 +27,7 @@
 						<br>
 					
 						<a href= "{{ action('CampaignController@eventDetail', [$activeevent->id])}}" class="btn btn-success btn-lg" style="font-size: 14px" id="button-learn_more"> Learn More </a>
-						@if ($activeevent->name == 'Junior Achievement Omaha Golf Campaign - 2016')
+						@if ($activeevent->id == 1)
 							<a href= "{{action('CampaignController@createTeam', [$activeevent->id])  }}" class="btn btn-success btn-lg" style="font-size: 14px" id="button-register_now"> Register Now </a>
 						@else
 							<a href= "{{action('CampaignController@createTeam', [$activeevent->id])  }}" class="btn btn-success btn-lg" style="font-size: 14px" id="button-create_team"> Create Team </a>
