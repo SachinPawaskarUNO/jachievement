f5cb1b9bd8d0403e8d2579bdb25b0b78fdc1512d
Freddy Chew <Freddy Chew>
2016-11-13 12:44:11 -0600

    update Report Donations2

--- app/Http/Controllers/ReportsController.php
+++ app/Http/Controllers/ReportsController.php
@@ -34,6 +34,8 @@ class ReportsController extends Controller
            
              ->groupBy(DB::raw('mon'))
              ->get();
+
+
 // return $chart_data;
 $data=array();
 
@@ -43,13 +45,10 @@ foreach($chart_data as $chart_values)
 
 }
              $chart_results = array_column($data, 'sum');
+            $chart_months = array_column($data, 'mon');    
 
-             // return $chart_results;
-//            $chart_data = json_encode($chart_results);
-// return json_encode($chart_results);
 
-        // return view('reports.donation',compact('donors'))->with('chart_results',json_encode($chart_results,JSON_NUMERIC_CHECK));
-        return view('reports.donation',compact('donors'))->with('chart_results',json_encode($chart_results,JSON_NUMERIC_CHECK));
+        return view('reports.donation',compact('donors'))->with('chart_results',json_encode($chart_results,JSON_NUMERIC_CHECK))->with('chart_months',json_encode($chart_months,JSON_NUMERIC_CHECK));
 
     }
 }
