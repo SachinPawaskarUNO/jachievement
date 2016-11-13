b8c3f95499e5a8c72e8eae645c5a4667d0c6127d
Freddy Chew <Freddy Chew>
2016-11-13 12:35:20 -0600

    update Report Donations2

--- app/Http/Controllers/ReportsController.php
+++ app/Http/Controllers/ReportsController.php
@@ -28,11 +28,11 @@ class ReportsController extends Controller
 
          $chart_data= DB::table('donations')
              // ->select(DB::raw('SUM(donations.amount) as sum,month(donations.created_at) as month'))
-                ->select(DB::raw('SUM(donations.amount) as sum'))
+                ->select(DB::raw('SUM(donations.amount) as sum, to_char(donations.created_at,\'Mon\') as mon'))
              // ->where('year(donations.created_at)','=','2016')
                // ->where('year(donations.created_at)','=','2016')
            
-             ->groupBy(DB::raw('EXTRACT(MONTH FROM TIMESTAMP \'donations.created_at\')')
+             ->groupBy(DB::raw('mon'))
              ->get();
 // return $chart_data;
 $data=array();
