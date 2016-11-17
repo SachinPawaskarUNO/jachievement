f4b4acdc5634e969a8e670df5bb55ebb2b9e920e
jmiverson <jiverson@unomaha.edu>
2016-11-16 20:24:17 -0600

    adding tracker database

--- config/database.php
+++ config/database.php
@@ -80,6 +80,18 @@ return [
             'schema'    => 'public',
         ],
 
+        'tracker' => [
+            'driver'    => 'pgsql',
+            'host'      => env('DB_HOST', 'localhost'),
+            'port'      => env('DB_PORT', '5432'),
+            'database'  => env('DB_DATABASE', 'forge'),
+            'username'  => env('DB_USERNAME', 'forge'),
+            'password'  => env('DB_PASSWORD', ''),
+            'charset'   => 'utf8',
+            'prefix'    => '',
+            'schema'    => 'public',
+        ],
+
         'sqlsrv' => [
             'driver'    => 'sqlsrv',
             'host'      => env('DB_HOST', 'localhost'),
