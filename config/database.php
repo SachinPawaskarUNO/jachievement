2392b957c369fafaa369851d21db1eec5f52aca7
jmiverson <jiverson@unomaha.edu>
2016-11-19 15:58:24 -0600

    Fixing Heroku w/ database.php file

--- config/database.php
+++ config/database.php
@@ -81,7 +81,7 @@ return [
         ],
 
         'tracker' => [
-            'driver'    => 'mysql',
+            'driver'    => env('DB_DRIVER', 'mysql'),
             'host'      => env('DB_HOST', 'localhost'),
             'port'      => env('DB_PORT', '3306'),
             'database'  => env('DB_DATABASE', 'jachievement'),
