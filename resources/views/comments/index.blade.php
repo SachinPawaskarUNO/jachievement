43b5ee81c4c3abe2f27649bcfbf5111e7cf53a7a
arpitaadhikari <arpitaadhikari@unomaha.edu>
2016-11-22 12:19:57 -0600

    css fix

--- resources/views/comments/index.blade.php
+++ resources/views/comments/index.blade.php
@@ -4,7 +4,7 @@
 
     <div class="container">
         <div class="row">
-            <div class="col-md-11">
+            <div class="col-md-12">
             <div class="panel panel-default">
                 <div class="panel-heading"  style="background-color:#5cb85c !important;">
 
