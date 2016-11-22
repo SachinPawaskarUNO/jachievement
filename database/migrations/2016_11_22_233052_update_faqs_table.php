5613fa324ab43ea048e5a0f22149937814624b51
Ninh Nguyen <nhnguyen@unomaha.edu>
2016-11-22 17:39:50 -0600

    change draft flag to null

---
+++ database/migrations/2016_11_22_233052_update_faqs_table.php
@@ -0,0 +1,33 @@
+<?php
+
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class UpdateFaqsTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::table('maven_faqs', function($table)
+        {
+            $table->boolean('draft_flag')->nullable()->change();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::table('maven_fags', function($table)
+        {
+            $table->dropColumn('draft_flag');
+        });
+    }
+}
