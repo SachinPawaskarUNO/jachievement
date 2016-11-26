e31fd0e232cf6e2abba3451cccf0dbcba8072f3b
Ninh Nguyen <nhnguyen@unomaha.edu>
2016-11-26 17:33:46 -0600

    enhance UI

--- resources/views/maven/view.blade.php
+++ resources/views/maven/view.blade.php
@@ -1,3 +1,7 @@
+@extends('maven.layout')
+@include('common.nav')
+
+@section('content')
 <!doctype html>
 <html lang="en" class="no-js">
 <head>
@@ -8,15 +12,17 @@
     <link rel="stylesheet" href="{{ URL::asset('css/reset.css') }}" />
     <link rel="stylesheet" href="{{ URL::asset('css/style.css') }}" />
     <link href='http://fonts.googleapis.com/css?family=Open+Sans:400,300,600,700' rel='stylesheet' type='text/css'>
-    <title>FAQ Template | CodyHouse</title>
+    <title>Frequent Asked Questions</title>
 </head>
 <body>
 <header>
-    <h1>FAQ Template</h1>
+    <h1>Frequently Asked Question</h1>
 </header>
 <section class="cd-faq">
     <ul class="cd-faq-categories">
-        <li><a class="selected" href="#basics">Basics</a></li>
+        @foreach($maven_tags as $maven_tag)
+        <li><a href="#basics">{{$maven_tag->tag}}</a></li>
+        @endforeach
     </ul> <!-- cd-faq-categories -->
 
 {{--    <div class="cd-faq-items">
@@ -34,18 +40,21 @@
     </div> <!-- cd-faq-items -->--}}
 
     <div class="cd-faq-items">
+        @foreach($maven_tags as $maven_tag)
         <ul id="basics" class="cd-faq-group">
-            <li class="cd-faq-title"><h2>Educator</h2></li>
+            <li class="cd-faq-title"><h2>{{$maven_tag->tag}}</h2></li>
             @foreach($maven_items as $maven_item)
+                @if ($maven_item->tag == $maven_tag->tag)
                 <li>
                     <a class="cd-faq-trigger" href="#0">{{$maven_item->question}}</a>
                     <div class="cd-faq-content">
                         <p>{{$maven_item->answer}}</p>
-                        <p>{{$maven_item->tag}}</p>
                     </div> <!-- cd-faq-content -->
                 </li>
+                @endif
             @endforeach
-        </ul> <!-- cd-faq-group -->
+        </ul>
+        @endforeach<!-- cd-faq-group -->
     </div> <!-- cd-faq-items -->
 
     <a href="#0" class="cd-close-panel">Close</a>
@@ -56,3 +65,4 @@
 
 </body>
 </html>
+@endsection
