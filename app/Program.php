6aabf179b8dc4600205d1703d2ebae983aec22bb
Jaspreet Sahota <jsahota@unomaha.edu>
2016-12-02 14:41:28 -0600

    Bug fix for events and programs to hide deleted stuff

--- app/Program.php
+++ app/Program.php
@@ -3,10 +3,13 @@
 namespace App;
 
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\SoftDeletes;
 
 class Program extends Model
 {
+    use SoftDeletes;
     protected $table = 'programs';
+    protected $dates = ['deleted_at'];
     protected $fillable = [
 
         'id','name','implementation', 'entrepreneurship','financial_readiness','work_readiness','image','program_url','description','grade_id'
