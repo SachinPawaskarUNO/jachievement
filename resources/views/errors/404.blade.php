c3e64d3a2c99d05b30b745b2c5d9a797be200245
arpitaadhikari <arpitaadhikari@unomaha.edu>
2016-12-03 10:04:28 -0600

    Worked on client feedback for donor intro page

---
+++ resources/views/errors/404.blade.php
@@ -0,0 +1,11 @@
+@extends('layout.app')
+
+@section('content')
+
+    <div class="404-error">
+        <h1>We couldn't find that page.</h1>
+        <br>
+        The page you requested wasn't found.
+    </div>
+
+@endsection
