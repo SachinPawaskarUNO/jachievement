9ae003b876bbd1ee7779aea62ea0fea414336313
Ninh Nguyen <nhnguyen@unomaha.edu>
2016-12-10 13:17:30 -0600

    fix

--- database/seeds/UsersRolesPermissions.php
+++ database/seeds/UsersRolesPermissions.php
@@ -12,7 +12,7 @@ class UsersTableSeeder extends Seeder {
         DB::table('users')->delete();
         User::create([  'first_name' => 'John', 'last_name' => 'Healey', 'password' => bcrypt('secret'), 'email' => 'jhealey@jaomaha.net', 'active' => true,
             'created_by' => 'System', 'updated_by' => 'System', 'created_at' => date_create(), 'updated_at' => date_create()]);
-        User::create([  'first_name' => 'Roxanne', 'last_name' => 'Mulvey', 'password' => bcrypt('secret'), 'email' => 'mculvey@jaomaha.net', 'active' => true,
+        User::create([  'first_name' => 'Roxanne', 'last_name' => 'Mulvey', 'password' => bcrypt('secret'), 'email' => 'mulvey@jaomaha.net', 'active' => true,
             'created_by' => 'System', 'updated_by' => 'System', 'created_at' => date_create(), 'updated_at' => date_create()]);
         User::create([  'first_name' => 'Robert', 'last_name' => 'Guillard', 'password' => bcrypt('secret'), 'email' => 'rguillard@unomaha.edu', 'active' => true,
             'created_by' => 'System', 'updated_by' => 'System', 'created_at' => date_create(), 'updated_at' => date_create()]);
